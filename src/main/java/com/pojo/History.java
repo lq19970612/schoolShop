722ec559d818ac369345604ad8f46df4cd782396
lq19970612 <1318347037@qq.com>
2019-03-18 20:46:24 +0800

    添加History Pojo

---
+++ src/main/java/com/pojo/History.java
@@ -0,0 +1,47 @@
+package com.pojo;
+
+
+public class History {
+
+  private long historyId;
+  private long userId;
+  private long goodsId;
+  private java.sql.Timestamp historyDate;
+
+
+  public long getHistoryId() {
+    return historyId;
+  }
+
+  public void setHistoryId(long historyId) {
+    this.historyId = historyId;
+  }
+
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+
+  public long getGoodsId() {
+    return goodsId;
+  }
+
+  public void setGoodsId(long goodsId) {
+    this.goodsId = goodsId;
+  }
+
+
+  public java.sql.Timestamp getHistoryDate() {
+    return historyDate;
+  }
+
+  public void setHistoryDate(java.sql.Timestamp historyDate) {
+    this.historyDate = historyDate;
+  }
+
+}

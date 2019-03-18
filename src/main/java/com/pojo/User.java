9de48458ce4892ae7ae26d5e1ceec25e71fbc2b8
Liuquan <1318347037@qq.com>
2019-03-18 11:06:29 +0800

    pojo上传

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,37 @@
+package com.pojo;
+
+
+public class User {
+
+  private long userId;
+  private String userName;
+  private String userPassword;
+
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+
+  public String getUserPassword() {
+    return userPassword;
+  }
+
+  public void setUserPassword(String userPassword) {
+    this.userPassword = userPassword;
+  }
+
+}
